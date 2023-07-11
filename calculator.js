7104182677e95118a6228576480596173980e4bb
Yasmeen Othman <yasmeen.othman20@gmail.com>
2023-07-11 13:29:36 +0300

    starting with js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
-	
-	
+let x = document.getElementById("input");
+let output = document.getElementById("output");
+let keys = document.querySelectorAll(".key");
 
+keys.forEach((key) => {
+  key.addEventListener("click", function (e) {
+    console.log(e.target.innerText);
+  });
+});
